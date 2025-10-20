88a51c480ba332c31776a9f04fea73fb6203684c
Pham Tran Thien Dang <phamtranthiendang308@gmail.com>
2025-10-20 12:18:16 +0700

    Tao cau truc do an (lan 1)

---
+++ src/main/java/mocviet/MocVietFurnitureStoreApplication.java
@@ -0,0 +1,13 @@
+package mocviet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocVietFurnitureStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocVietFurnitureStoreApplication.class, args);
+	}
+
+}
